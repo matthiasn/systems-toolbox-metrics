eddc4a54c283f3c650f6b70e071d9cbead3375d7
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-01-03 17:41:23 +0100

    v0.5.1: metrics moved from systems-toolbox

---
+++ changes.md
@@ -0,0 +1,7 @@
+## v0.5.1 - January 3rd, 2016
+
+```clojure
+[matthiasn/systems-toolbox-metrics "0.5.1"]
+```
+
+* Metrics component moved into separate repos
---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject matthiasn/systems-toolbox-metrics "0.5.1"
+  :description "Basic JVM stats for systems-toolbox apps"
+  :url "https://github.com/matthiasn/systems-toolbox"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj" "src/cljs" "src/cljc"]
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.reader "1.0.0-alpha1"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.ow2.asm/asm-all "5.0.4"]
+                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
+                 [matthiasn/systems-toolbox-ui "0.5.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [io.aviso/pretty "0.1.20"]
+                 [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]]
+
+  :plugins [[lein-codox "0.9.1" :exclusions [org.clojure/clojure]]
+            [lein-cljsbuild "1.1.2"]])
