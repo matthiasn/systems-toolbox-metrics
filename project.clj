4e8ca706949b4f75a3e6c6c69fee2b7b9076eb53
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-11-25 14:09:02 +0100

    v0.6.1: latest deps

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject matthiasn/systems-toolbox-metrics "0.6.1-alpha4"
+(defproject matthiasn/systems-toolbox-metrics "0.6.1"
   :description "Basic JVM stats for systems-toolbox apps"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
 
-  :dependencies [[matthiasn/systems-toolbox-ui "0.6.1-alpha11"]
+  :dependencies [[matthiasn/systems-toolbox-ui "0.6.1"]
                  [org.clojure/tools.logging "0.3.1"]])
